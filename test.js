eca9bfdccb9aa41d75ff5eb6fa465f06f247c792
ghn9264 <ghn9264@gmail.com>
2020-04-30 17:53:48 +0800

    o

--- test.js
+++ test.js
@@ -29,15 +29,23 @@
 // let str = "<br>123<br>"
 // str = str.replace(/<br\s*.?>/g,'/n')
 // console.log(str);
-let str = "[CQ:at,qq=441550253] \r\n[CQ:image,file=8AE48CCAE34120C2580E11149053C29A.jpg]\r\n本群第3位 签到完成\r\n排名：9161\r\n资历 +10\r\n运势：大战 攒人品"
-let qq;
-let ys1;
-let ys2;
-let a = str.split(' ')
-console.log(a);
-qq = a[0].replace(/[^0-9]/ig,"");
-console.log(qq);
-ys1 = a[3].split("：")[1]
-ys2 = a[4]
-console.log(ys1,ys2);
+// let str = "[CQ:at,qq=441550253] \r\n[CQ:image,file=8AE48CCAE34120C2580E11149053C29A.jpg]\r\n本群第3位 签到完成\r\n排名：9161\r\n资历 +10\r\n运势：大战 攒人品"
+// let qq;
+// let ys1;
+// let ys2;
+// let a = str.split(' ')
+// console.log(a);
+// qq = a[0].replace(/[^0-9]/ig,"");
+// console.log(qq);
+// ys1 = a[3].split("：")[1]
+// ys2 = a[4]
+// console.log(ys1,ys2);
+let str = "本职业|明教"
+if (str.includes("本职业|")) {
+    console.log("123");
+    
+}else{
+    console.log(str.includes("本职业|"));
+    
+}
 
